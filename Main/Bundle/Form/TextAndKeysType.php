8ab386692d5926b92189e7fe12df0dcda0597dd0
thomas <devos.thom@gmail.com>
2014-05-26 21:48:16 +0200

    First commit

---
+++ Main/Bundle/Form/TextAndKeysType.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Keywords\Main\Bundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+
+class TextAndKeysType extends AbstractType{
+
+    public function getName(){
+        return 'textAndKeys';
+    }
+
+}
---
+++ Main/Bundle/KeywordsMainBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Keywords\Main\Bundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class KeywordsMainBundle extends Bundle
+{
+}
