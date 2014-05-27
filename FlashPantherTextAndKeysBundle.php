26011954397ca27025b797482f441cd18e92537f
tdevos <devos.thom@gmail.com>
2014-05-27 21:49:18 +0200

    Update and rename KeywordsMainBundle.php to FlashPantherTextAndKeysBundle.php

---
+++ FlashPantherTextAndKeysBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace FlashPanther\TextAndKeys;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class FlashPantherTextAndKeysBundle extends Bundle
+{
+}
--- KeywordsMainBundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Keywords\Main\Bundle;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class KeywordsMainBundle extends Bundle
-{
-}
